578abac2d50d3469e60e557d941ec00c81e1d886
elblaus <ludvig@ludvigelblaus.com>
2022-10-16 00:03:36 +0200

    yes

--- config/nvim/lua/ludo/plugins.lua
+++ config/nvim/lua/ludo/plugins.lua
@@ -2,16 +2,16 @@
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
---    use 'airblade/vim-gitgutter'
---    use 'lunarvim/colorschemes'
---    use {
---	    'akinsho/bufferline.nvim', 
---	    tag = "v2.*", 
---	    requires = 'kyazdani42/nvim-web-devicons'
- --   }
---    use {
---	    'nvim-telescope/telescope.nvim', tag = '0.1.0',
---	    requires = { {'nvim-lua/plenary.nvim'} }
- --   }
+    use 'airblade/vim-gitgutter'
+    use 'lunarvim/colorschemes'
+    use {
+	    'akinsho/bufferline.nvim', 
+    tag = "v2.*", 
+	    requires = 'kyazdani42/nvim-web-devicons'
+    }
+    use {
+	    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+    }
     use { 'davidgranstrom/scnvim' }
 end)
