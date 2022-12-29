6c977e2506863afd036dcd2053a151495df4ac85
elblaus <ludvig@ludvigelblaus.com>
2022-12-29 04:53:45 +0100

    kitty tuning

--- config/nvim/lua/ludo/plugins.lua
+++ config/nvim/lua/ludo/plugins.lua
@@ -1,41 +1,49 @@
-vim.cmd [[packadd packer.nvim]]
+--vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use 'airblade/vim-gitgutter'
     use 'lunarvim/colorschemes'
---    use {
---	    'akinsho/bufferline.nvim', 
---        tag = "v3.*",
---	    requires = 'kyazdani42/nvim-web-devicons'
---    }
     use {
 	    'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} }
     }
     use { 'davidgranstrom/scnvim' }
     use('nvim-treesitter/nvim-treesitter', { run = 'TSUpdate'})
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-        }
-    }
+--    use {
+--        'VonHeikemen/lsp-zero.nvim',
+--        requires = {
+--            -- LSP Support
+--            {'neovim/nvim-lspconfig'},
+--            {'williamboman/mason.nvim'},
+--            {'williamboman/mason-lspconfig.nvim'},
+--
+--            -- Autocompletion
+--            {'hrsh7th/nvim-cmp'},
+--            {'hrsh7th/cmp-buffer'},
+--            {'hrsh7th/cmp-path'},
+--            {'saadparwaiz1/cmp_luasnip'},
+--            {'hrsh7th/cmp-nvim-lsp'},
+--            {'hrsh7th/cmp-nvim-lua'},
+--
+--            -- Snippets
+--            {'L3MON4D3/LuaSnip'},
+--            {'rafamadriz/friendly-snippets'},
+--        }
+--    }
+--    use {
+--        'hrsh7th/nvim-cmp',
+--        requires = {
+--            'hrsh7th/cmp-buffer',
+--            'hrsh7th/cmp-nvim-lsp',
+--            'hrsh7th/cmp-path',
+--            'quangnguyen30192/cmp-nvim-tags',
+--            'saadparwaiz1/cmp_luasnip',
+--            'onsails/lspkind-nvim',
+--            'kyazdani42/nvim-web-devicons'
+--        },
+--        --config = r'nvim-cmp'
+--  }
 end)
 
 
--- config/nvim/lua/ludo/scnvim.lua
+++ config/nvim/lua/ludo/scnvim.lua
@@ -24,17 +24,18 @@ scnvim.setup({
       color = 'IncSearch',
     },
   },
-  postwin = {
-      scrollback = '10000',
-      horizontal = true,
-      float = {
-        enabled = true,
-        width = vim.o.columns,
-        height = 24,
-        row = vim.o.lines,
-      },
-  },
+--  postwin = {
+--      scrollback = '10000',
+--      horizontal = true,
+--      float = {
+--        enabled = true,
+--        width = vim.o.columns,
+--        height = 24,
+--        row = vim.o.lines,
+--      },
+--  },
 })
+
 
 --- sketch file 
 vim.keymap.set('n', '<leader>ss', function()
@@ -47,3 +48,65 @@ vim.keymap.set('n', '<leader>ss', function()
     vim.api.nvim_command("e " .. fileName)
     vim.api.nvim_command("SCNvimStart")
 end)
+
+-- comp
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+local lspkind = require'lspkind'
+local luasnip = require'luasnip'
+local cmp = require'cmp'
+
+local next_item = function(fallback)
+  if cmp.visible() then
+    cmp.select_next_item()
+  elseif has_words_before() then
+    cmp.complete()
+  else
+    fallback()
+  end
+end
+
+local prev_item = function(fallback)
+  if cmp.visible() then
+    cmp.select_prev_item()
+  else
+    fallback()
+  end
+end
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      require'luasnip'.lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  completion = {
+    keyword_length = 3,
+  },
+  window = {
+    documentation = false,
+  },
+  experimental = {
+    native_menu = false,
+    ghost_text = false,
+  },
+  formatting = {
+    format = lspkind.cmp_format({preset = 'default', mode = 'symbol_text', maxwidth = 50})
+  },
+  mapping = {
+    -- ['<C-j>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(next_item, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(prev_item , { 'i', 's' }),
+    ['<C-n>'] = cmp.mapping(next_item, { 'i', 's' }),
+    ['<C-p>'] = cmp.mapping(prev_item , { 'i', 's' }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'tags' },
+    { name = 'path' },
+    { name = 'buffer' },
+    { name = 'luasnip' },
+  },
+}
