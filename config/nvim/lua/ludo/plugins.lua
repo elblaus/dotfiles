c9f22f1372047b671107cc787142f046b83fb3ba
elblaus <ludvig@ludvigelblaus.com>
2023-08-14 12:04:15 +0200

    comments

--- config/nvim/lua/ludo/plugins.lua
+++ config/nvim/lua/ludo/plugins.lua
@@ -44,4 +44,10 @@ return require('packer').startup(function(use)
             {'rafamadriz/friendly-snippets'},
         }
     }
+    use {
+    'numToStr/Comment.nvim',
+    config = function()
+        require('Comment').setup()
+    end
+    }
 end)
