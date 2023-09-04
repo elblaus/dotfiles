b1fe6344d90f965937c59386e6dce06dd1fe8ea8
elblaus <ludvig@ludvigelblaus.com>
2023-09-04 19:11:09 +0200

    cleanup

--- aliases-generic.zsh
+++ aliases-generic.zsh
@@ -3,12 +3,6 @@ alias vim="nvim"
 export VISUAL=nvim
 export EDITOR=nvim
 
-# NOTES
-alias note="~/Dropbox/linux/shell-scripts/note.sh"
-notes() {
-    nvim $(find ~/Dropbox/txt/notebook/*.md | fzf --preview="cat {}")
-}
-
 # GIT
 alias st="git status"
 alias cm="git commit -m"
